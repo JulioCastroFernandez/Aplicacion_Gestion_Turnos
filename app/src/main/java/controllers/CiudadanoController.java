e513accf53718e9161e3666ee9873a1fb1e24fb3
Francisco José Morcillo Suárez <francisco.morcillo89@gmail.com>
2025-06-02 23:27:25 +0200

    añadido método listar ciudadanos

--- app/src/main/java/controllers/CiudadanoController.java
+++ app/src/main/java/controllers/CiudadanoController.java
@@ -3,20 +3,22 @@ package controllers;
 import entities.Ciudadano;
 import persistence.ControladorPersistencia;
 
+import java.util.List;
+
 public class CiudadanoController {
     ControladorPersistencia cp = new ControladorPersistencia();
 
     // METODO para crear un ciudadano
-    public void crearCiudadano(String dni, String nombre, String apellido){
+    public void crearCiudadano(String dni, String nombre, String apellido) {
         // Verificamos si los campos están vacíos
-        if(dni.isBlank() || nombre.isBlank() || apellido.isBlank()){
+        if (dni.isBlank() || nombre.isBlank() || apellido.isBlank()) {
             System.out.println("El dni, nombre y apellidos son obligatorios.");
             return;
         }
 
         // Verificamos si ya existe un ciudadano con ese DNI
         Ciudadano existe = cp.buscarCiudadanoPorDNI(dni);
-        if(existe != null){
+        if (existe != null) {
             System.err.println("Ya existe un ciudadano con el DNI: " + dni);
             return;
         }
@@ -28,5 +30,18 @@ public class CiudadanoController {
         ciudadano.setApellido(apellido);
         cp.crearCiudadano(ciudadano);
         System.out.println("Ciudadano creado con éxito: " + ciudadano);
+    }
+
+    // METODO para listar los ciudadanos
+
+    public List<Ciudadano> listarCiudadanos() {
+        List<Ciudadano> lista = cp.buscarTodosLosCiudadanos();
+        if (lista.isEmpty()) {
+            System.out.println("No se han encontrado ciudadanos");
+        } else {
+            System.out.println("Listado: ");
+            lista.forEach(System.out::println);
+        }
+        return lista;
     }
 }
